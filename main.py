3497209c444be75eee21da112dc272a8aa48c0bc
PankajSingh0018 <singh.pankaj0018@gmail.com>
2023-09-13 17:17:16 +0530

    Added model trainer file

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from TextSummarizer.pipeline.Stage_01_data_ingestion import DataIngestionTrainingPipeline
 from TextSummarizer.pipeline.Stage_02_data_validation import DataValidationTrainingPipeline
+from TextSummarizer.pipeline.Stage_03_data_transformation import DataTransformationTrainingPipeline
 from TextSummarizer import logger
+
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -25,5 +27,18 @@ try:
     logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<< \n\nx========================================================================x")
 
 except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Tranformation Stage"
+
+try:
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<< \n\nx========================================================================x")
+
+except Exception as e: 
     logger.exception(e)
     raise e
